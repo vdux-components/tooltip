30518d07a32cd399978ba1356e197a8949039362
Andrew Shaffer <andrew@weo.io>
2016-03-03 16:18:03 -0800

    update docs and use newer transition style

--- src/index.js
+++ src/index.js
@@ -2,30 +2,29 @@
  * Imports
  */
 
-import Transition from 'vdux-transition'
+import CSSTransition from 'vdux-css-transition'
 import Position from 'vdux-position'
 import element from 'vdux/element'
 import Hover from 'vdux-hover'
-import Delay from 'vdux-delay'
 
 /**
  * Tooltip
  */
 
 function render ({path, props, children}) {
-  const {message, delay = 0, placement = 'top', style = {}, space = 0} = props
+  const {message, delay = 0, transition} = props
 
   return (
     <span id={path}>
-      <Hover>
+      <Hover delay={delay}>
         {children}
         {
           hover => (
-            <Transition>
+            <CSSTransition timeout={transition}>
               {
                 hover && <InnerTooltip {...props} key='tooltip' near={path}>{message}</InnerTooltip>
               }
-            </Transition>
+            </CSSTransition>
           )
         }
       </Hover>
@@ -33,33 +32,16 @@ function render ({path, props, children}) {
   )
 }
 
-const InnerTooltip = {
-  render ({props, children}) {
-    const {delay, placement, near, space, style} = props
-    const {entering, leaving, didEnter, didLeave} = props.transition
+function InnerTooltip ({props, children}) {
+  const {placement = 'top', near, space = 0, style = {}} = props
 
-    console.log('tooltip', props.transition)
-
-    return (
-      <Delay time={delay}>
-        <Position placement={placement} near={near} space={space}>
-          <div class={[props.class, {entering, leaving}]} style={{...defaultStyle, ...style}}>
-            <Delay time={3000} onEnd={didEnter} />
-            {children}
-            {entering && 'ENTERING!!'}
-            {
-              leaving && (
-                <span>
-                  LEAVING!!
-                  <Delay time={3000} onEnd={didLeave} />
-                </span>
-              )
-            }
-          </div>
-        </Position>
-      </Delay>
-    )
-  }
+  return (
+    <Position placement={placement} near={near} space={space}>
+      <div class={props.class} style={{...defaultStyle, ...style}}>
+        {children}
+      </div>
+    </Position>
+  )
 }
 
 /**
